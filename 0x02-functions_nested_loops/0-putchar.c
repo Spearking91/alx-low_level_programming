876e1cdb8dc224c99ec4d3f809b4be89711aff04
Spearking91 <baninacquah@gmail.com>
2023-06-21 10:06:04 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - A program that prints a line
  * Return: 0 (success)
  */
 int main(void)
 {
-	printf("_putchar");
+	printf("\_putchar");
 	return (0);
 }
